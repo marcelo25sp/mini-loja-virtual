bedab4675294f884b945592a71220bde3ec2ba65
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-12 16:01:32 -0300

    Cria o construtor para a inicialização dos atributos e os métodos getters/setters da classe

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,11 +1,59 @@
 package entities;
 
 public class Produto {
-	
+
 	private int id;
 	private String nome;
 	private double preco;
 	private int quantidade;
 	private String categoria;
+
+	public Produto(int id, String nome, double preco, int quantidade, String categoria) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.categoria = categoria;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 
 }
