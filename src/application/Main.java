3a072f587fcc485b05b27472f09db1d5ac89e028
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-12 18:00:07 -0300

    Cria o primeiro esboço do menu de interação com o usuário na classe Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,22 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		System.out.println("====================================");
+		System.out.println("SISTEMA DE GERENCIAMENTO DE PRODUTOS");
+		System.out.println("====================================");
+		System.out.println();
+
+		System.out.println("1. Cadastrar produto:");
+		System.out.println("2. Listar produtos:");
+		System.out.println("3. Buscar produto por código:");
+		System.out.println("4. Editar produto:");
+		System.out.println("5. Remover produto:");
+		System.out.println("6. Filtrar produtos:");
+		System.out.println("0. Sair:");
+		
+		System.out.print("\nDigite a opção: ");
+		int opcao = sc.nextInt();
+
 		sc.close();
 
 	}
