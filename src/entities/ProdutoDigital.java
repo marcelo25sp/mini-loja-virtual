ebbfcc3efbf8718f1fea9f5fbe9551c85f8212b4
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-13 15:11:13 -0300

    Cria os atributos da classe ProdutoDigital e os adiciona ao construtor

--- src/entities/ProdutoDigital.java
+++ src/entities/ProdutoDigital.java
@@ -1,9 +1,14 @@
 package entities;
 
 public class ProdutoDigital extends Produto{
+	
+	private double tamanhoArquivo;
+	private double taxaDigital;
 
-	public ProdutoDigital(int id, String nome, double preco, int quantidade, String categoria) {
+	public ProdutoDigital(int id, String nome, double preco, int quantidade, String categoria, double tamanhoArquivo, double taxaDigital) {
 		super(id, nome, preco, quantidade, categoria);
+		this.tamanhoArquivo = tamanhoArquivo;
+		this.taxaDigital = taxaDigital;
 	}
 
 	@Override
