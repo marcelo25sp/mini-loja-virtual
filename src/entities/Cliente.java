954c1e7202ab34b4ab83d813c382135fe958dc35
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-13 18:14:08 -0300

    Implementa os atributos e o construtor da classe Cliente

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -2,4 +2,16 @@ package entities;
 
 public class Cliente {
 
+	private int id;
+	private String nome;
+	private String email;
+	private Carrinho carrinho;
+
+	public Cliente(int id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.carrinho = carrinho;
+	}
+
 }
