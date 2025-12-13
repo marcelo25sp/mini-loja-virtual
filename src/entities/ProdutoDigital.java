24f200c1f3563931bc680344fcf97481856eca9a
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-13 17:51:59 -0300

    Implementa os métodos getters na classe ProdutoDigital

--- src/entities/ProdutoDigital.java
+++ src/entities/ProdutoDigital.java
@@ -1,14 +1,23 @@
 package entities;
 
-public class ProdutoDigital extends Produto{
-	
+public class ProdutoDigital extends Produto {
+
 	private double tamanhoArquivo;
 	private double taxaDigital;
 
-	public ProdutoDigital(int id, String nome, double preco, int quantidade, String categoria, double tamanhoArquivo, double taxaDigital) {
+	public ProdutoDigital(int id, String nome, double preco, int quantidade, String categoria, double tamanhoArquivo,
+			double taxaDigital) {
 		super(id, nome, preco, quantidade, categoria);
 		this.tamanhoArquivo = tamanhoArquivo;
 		this.taxaDigital = taxaDigital;
+	}
+
+	public double getTamanhoArquivo() {
+		return tamanhoArquivo;
+	}
+
+	public double getTaxaDigital() {
+		return taxaDigital;
 	}
 
 	@Override
