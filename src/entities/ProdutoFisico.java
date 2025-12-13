bfcf47bdad6c363a79cdf79a4f6d1173b37279fc
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-13 15:05:47 -0300

    Cria os métodos getters da classe ProdutoFisico

--- src/entities/ProdutoFisico.java
+++ src/entities/ProdutoFisico.java
@@ -1,19 +1,28 @@
 package entities;
 
 public class ProdutoFisico extends Produto {
-	
+
 	private double peso;
 	private double custoFrete;
 
-	public ProdutoFisico(int id, String nome, double preco, int quantidade, String categoria, double peso, double custoFrete) {
+	public ProdutoFisico(int id, String nome, double preco, int quantidade, String categoria, double peso,
+			double custoFrete) {
 		super(id, nome, preco, quantidade, categoria);
 		this.peso = peso;
 		this.custoFrete = custoFrete;
 	}
 
+	public double getPeso() {
+		return peso;
+	}
+
+	public double getCustoFrete() {
+		return custoFrete;
+	}
+
 	@Override
 	public double calcularPrecoFinal() {
-		return 0;
+		return 0; // em construção
 	}
 
 }
