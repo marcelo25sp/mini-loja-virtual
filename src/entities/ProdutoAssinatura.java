fb49fc7313f6ee24117a5d0333a6fb52ee15d992
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-13 15:17:29 -0300

    Cria os atributos da classe ProdutoAssinatura e os adiciona ao construtor

--- src/entities/ProdutoAssinatura.java
+++ src/entities/ProdutoAssinatura.java
@@ -1,9 +1,14 @@
 package entities;
 
 public class ProdutoAssinatura extends Produto {
+	
+	private int meses;
+	private double descontoPorMes;
 
-	public ProdutoAssinatura(int id, String nome, double preco, int quantidade, String categoria) {
+	public ProdutoAssinatura(int id, String nome, double preco, int quantidade, String categoria, int meses, double descontoPorMes) {
 		super(id, nome, preco, quantidade, categoria);
+		this.meses = meses;
+		this.descontoPorMes = descontoPorMes;
 	}
 
 	@Override
