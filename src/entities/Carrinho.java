f96c9957b4bc98cca8efe329663f52b98ce76fdd
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-13 18:24:00 -0300

    Cria a função para adicionar produtos no carrinho

--- src/entities/Carrinho.java
+++ src/entities/Carrinho.java
@@ -1,5 +1,14 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrinho {
+
+	private List<Produto> itens = new ArrayList<>();
+	
+	public void adicionarProduto(Produto produto) {
+		itens.add(produto);
+	}
 
 }
