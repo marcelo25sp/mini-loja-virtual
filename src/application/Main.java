d0d4744232d2b646a61535476af97fe08677d412
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-12-13 14:10:21 -0300

    Implementa a estrutura switch-case e cria o esboço inicial de cada caso

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int opcao = -1; // atribuição provisória a variável para forçar e entrada no looping
-		
-		//looping de interação com o usuário
+
+		// looping de interação com o usuário
 		while (opcao != 0) {
 
 			System.out.println("====================================");
@@ -29,6 +29,35 @@ public class Main {
 
 			System.out.print("\nDigite a opção: ");
 			opcao = sc.nextInt();
+
+			switch (opcao) {
+
+			case 1:
+				System.out.println("Cadastrar Produto: (em construção)");
+				break;
+			case 2:
+				System.out.println("Listar produtos: (em construção)");
+				break;
+			case 3:
+				System.out.println("Buscar produtos por código: (em construção)");
+				break;
+			case 4:
+				System.out.println("Editar produto: (em construção)");
+				break;
+			case 5:
+				System.out.println("Remover produto: (em construção)");
+				break;
+			case 6:
+				System.out.println("Filtrar produtos: (em construção)");
+				break;
+			case 0:
+				System.out.println("Saindo do programa... (em construção)");
+				break;
+			default:
+				System.out.println("Opção inválida! Digite novamente: (em construção)");
+				break;
+
+			}
 
 		}
 
