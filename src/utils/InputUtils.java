150783a7fff3c7769473f991bb6d32f8c0a6ec6b
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-01-23 23:42:10 -0300

    Cria a função para ler e validar números inteiros

--- src/utils/InputUtils.java
+++ src/utils/InputUtils.java
@@ -1,5 +1,24 @@
 package utils;
 
+import java.util.Scanner;
+
 public class InputUtils {
+	
+	public static int lerInt(Scanner sc, String mensagem) {
+		int valor;
+		
+		while(true) {
+			System.out.print(mensagem);
+			if(sc.hasNextInt()) {
+				valor = sc.nextInt();
+				sc.nextLine(); // limpa o buffer
+				return valor;
+			}else {
+				System.out.println("Entrada inválida. Digite um número inteiro.");
+				sc.nextLine();
+			}
+		}
+		
+	}
 
 }
