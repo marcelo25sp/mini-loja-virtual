3f73fc674688b8a7195480acffa7f9d77ea95b5d
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-02-15 09:13:31 -0300

    Implementa teste unitário para ProdutoDigital

--- src/test/ProdutoTest.java
+++ src/test/ProdutoTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import entities.ProdutoDigital;
 import entities.ProdutoFisico;
 import entities.enums.Categoria;
 
@@ -18,6 +19,17 @@ public class ProdutoTest {
 		
 		// (1500.0 * 2) + 20.0 = 3020.00
 		assertEquals(3020.0, total);
-	}	
+	}
+	
+	@Test
+	void deveCalcularPrecoFinalProdutoDigital() {
+		
+		ProdutoDigital produto = new ProdutoDigital("Plano DevNet", 50.0, 1, Categoria.SERVICO, 200, 10);
+		
+		double total = produto.calcularPrecoFinal();
+		
+		// (50.0 * 1) + 10.0 = 60.0
+		assertEquals(60.0, total);
+	}
 
 }
